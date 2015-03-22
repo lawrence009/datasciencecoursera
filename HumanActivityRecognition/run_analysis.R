c548b1a0b0e51ccf2ce33a656a80040089b208d6
Lawrence Chen <lawrence009@outlook.com>
2015-03-22 16:12:44 -0700

    result of analysis

---
+++ HumanActivityRecognition/run_analysis.R
@@ -0,0 +1,15 @@
+# R script to analyze the tidy data set prepared with HAR_ETS.R
+
+# comment out if you don't want to reload the data
+#source('HAR_ETS.R')
+
+# We only want measurements on the mean and standard deviation
+# select mean and std
+features.mean  <- grep('-mean', names(df), fixed=T)
+features.std <- grep('-std', names(df), fixed=T)
+
+df2 <- df[, c(2, features.mean, features.std)]
+
+activity.mean <- aggregate(df2[, 2:80], list(df$activity), mean)
+
+write.table(activity.mean, file="activity.means.txt", row.name=F)
