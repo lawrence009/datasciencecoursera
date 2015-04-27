bd8f181c4e171b9c0ac36671d16a55ecdbb24ec7
Lawrence Chen <lawrence009@outlook.com>
2015-04-27 16:34:13 -0700

    check which hospital is measured for outcome of care

--- rprog/Programming3/Programming3.R
+++ rprog/Programming3/Programming3.R
@@ -1,5 +1,11 @@
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+#
+hospital <- read.csv('hospital-data.csv')
+
+#first 10 column headers are the same as hospital.csv
+outcome <- read.csv('outcome-of-care-measures.csv', colClasses = 'character')
 
 outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
 
+#which hospital is measured for care of outcome
+measured <- hospital$Provider.Number %in% outcome$Provider.Number
