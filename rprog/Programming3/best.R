78675f10705f43ac8bf21fad2c7546e114c080ff
Lawrence Chen <lawrence009@outlook.com>
2015-04-27 21:56:58 -0700

    correct solution for best.R

--- rprog/Programming3/best.R
+++ rprog/Programming3/best.R
@@ -1,4 +1,4 @@
-#library(data.table)
+library(dplyr)
 
 best <- function(state, outcome) {
     #Read outcome data
@@ -16,18 +16,29 @@ best <- function(state, outcome) {
     #read full dataset, subset and transform
     df <- read.csv(url, colClasses = 'character')
     df <- df[, c(1, 2, 7, mort)] #(1)provider number, (2)hopspital name, (7)state
-    df[, 4:6] <- sapply(df[, 4:6], as.numeric)
+    df[, 4:6] <- suppressWarnings(sapply(df[, 4:6], as.numeric))
     colnames(df)[4:6] <- substring(colnames(df)[4:6], first = 46) #remove the first 46 charaters
    
     
-    ## Check that state and outcome are valid
+    ## Check that state param is valid
     if(!(state %in% df$State)) {
-        stop('invalid state')
+        stop('invalid param  state: ', state)
     }
    
-    acceptable <- c('heart attack', 'heart failure', 'pneumonia')
-    if(!(outcome %in% acceptable)) {
-        stop('invalid outcome')
+    acceptable <- colnames(df[4:6])
+    names(acceptable) <- c('heart attack', 'heart failure', 'pneumonia')
+
+    ## Check that outcome param is valid
+    if(!(outcome %in% names(acceptable))) {
+        stop('invalid param outcome: ', outcome)
     }
+
+
+    #none dpyrl solution
+    df <- df[df$State == state, c('Hospital.Name', 'State', acceptable[outcome])]
+    df <- df[order(df[1]), ] #order by states first
+    df[order(df[3]), 1][1] #then order by outcome and return the first row hospital name
+
+
 
 }
---
+++ rprog/Programming3/test.R
@@ -0,0 +1,11 @@
+print('test #1')
+print(best("TX", "heart attack") == "CYPRESS FAIRBANKS MEDICAL CENTER")
+
+print('test #2')
+print(best("TX", "heart failure") == "FORT DUNCAN MEDICAL CENTER")
+
+print('test #3')
+print(best("MD", "heart attack") == "JOHNS HOPKINS HOSPITAL, THE")
+
+print('test #4')
+print(best("MD", "pneumonia") == "GREATER BALTIMORE MEDICAL CENTER")
