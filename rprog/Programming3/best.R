1992c274cc17a49c2b3fcc72d873031938faf59a
Lawrence Chen <lawrence009@outlook.com>
2015-04-27 18:37:54 -0700

    solution for reading outcome data and checking for valid inputs

--- rprog/Programming3/best.R
+++ rprog/Programming3/best.R
@@ -1,34 +1,33 @@
-library(data.table)
+#library(data.table)
 
 best <- function(state, outcome) {
     #Read outcome data
     url <- 'outcome-of-care-measures.csv'
+
+    #which columns contain 30-Day Death (mortality) rate
     colheads <- as.character(read.csv(url, header = F, nrows = 1, stringsAsFactors = F))
-    
-    #which columns contain 30-Day Death
     mort <- grep('^Hospital 30-Day Death \\(Mortality', colheads)
     
-    df <- as.data.frame(read.csv(url, colClasses = "character"))
+    #trying to set the colClasses for mortality rate columns ahead of time doesn't seem to work 
+#     colClasses <- rep_len('character', length(colheads))
+#     colClasses[mort] <- 'numeric'
+#     df <- read.csv(url, colClasses = colClasses)
+    
+    #read full dataset, subset and transform
+    df <- read.csv(url, colClasses = 'character')
     df <- df[, c(1, 2, 7, mort)] #(1)provider number, (2)hopspital name, (7)state
     df[, 4:6] <- sapply(df[, 4:6], as.numeric)
-    colnames(df) <- c('provider_num', 'hospital', 'state', 'heart.attack','heart.failure', 'pneumonia')
-    
-    dt <- as.data.table(df)
-    #setNames(dt, c('hospital', 'state', 'heart attack', 'heart failure', 'pneumonia'))
-  
-    #rm(df, mort, outcomes)   
-    
-    acceptable <- c('heart attack', 'heart failure', 'pneumonia')
+    colnames(df)[4:6] <- substring(colnames(df)[4:6], first = 46) #remove the first 46 charaters
+   
     
     ## Check that state and outcome are valid
-    if(!(state %in% dt$state)) {
+    if(!(state %in% df$State)) {
         stop('invalid state')
     }
-    
+   
+    acceptable <- c('heart attack', 'heart failure', 'pneumonia')
     if(!(outcome %in% acceptable)) {
         stop('invalid outcome')
     }
 
-    ## Return hospital name in that state with lowest 30-day death rate
-    dt[dt$state == state]
 }
