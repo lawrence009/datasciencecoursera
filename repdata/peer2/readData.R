f53d76862b93a8bf4f6018f43a411fe95718a20c
Lawrence Chen <lawrence.chen@experian.com>
2015-05-15 15:13:58 -0700

    read unique EVTYPE

--- repdata/peer2/readData.R
+++ repdata/peer2/readData.R
@@ -7,7 +7,19 @@ sample <- read.csv(filename, nrows = 100)
 colClasses <- vapply(sample, class, 'character')
 
 #large data set; skip read if already exists in the environment
-if (!exists('dtbl')) {
-    dtbl <- as.data.table(read.csv(filename, colClasses = 'character'))
+if (!exists('dtl')) {
+    dtl <- as.data.table(read.csv(filename, na.strings = '', colClasses = 'character'))
 }
 
+
+
+sort(dtl[, unique(EVTYPE)])
+
+
+dtl[grepl('Summary', EVTYPE) & grepl('wind', REMARKS), REMARKS]
+
+dtl[grepl('Summary', EVTYPE) & !grepl('wind', REMARKS), REMARKS]
+
+dtl[1:10, as.Date(BGN_DATE, '%m/%d/%Y')]
+
+dtl[1:10, as.IDate(BGN_DATE, '%m/%d/%Y')]
