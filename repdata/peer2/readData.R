19d0c69b7502f3c1867929731ccbc7ef9dbbe831
Lawrence Chen <lawrence.chen@experian.com>
2015-05-15 16:46:46 -0700

    too many types of event

--- repdata/peer2/readData.R
+++ repdata/peer2/readData.R
@@ -12,14 +12,38 @@ if (!exists('dtl')) {
 }
 
 
-
-sort(dtl[, unique(EVTYPE)])
-
-
-dtl[grepl('Summary', EVTYPE) & grepl('wind', REMARKS), REMARKS]
-
-dtl[grepl('Summary', EVTYPE) & !grepl('wind', REMARKS), REMARKS]
-
 dtl[1:10, as.Date(BGN_DATE, '%m/%d/%Y')]
 
 dtl[1:10, as.IDate(BGN_DATE, '%m/%d/%Y')]
+
+
+sort(dtl[, unique(EVTYPE)])
+
+dtl[, EVTYPE:=toupper(EVTYPE)]
+
+#VOLCANIC ASH
+dtl[grepl('VO', EVTYPE), EVTYPE:='VOLCANIC ASH']
+
+#WATERSPOUT
+
+#HUNDERSTORM WIND
+dtl[grepl('Summary', EVTYPE) & grepl('[Tt]hunderstorm.*[Ww]ind', REMARKS), EVTYPE:='THUNDERSTORM WIND']
+
+#HAIL
+dtl[grepl('Summary', EVTYPE) & grepl('[Hh]ail', REMARKS), EVTYPE:='HAIL']
+
+#FLASH FLOOD
+dtl[grepl('Summary', EVTYPE) & grepl('[Ff]lash flood', REMARKS), EVTYPE:='FLASH FLOOD']
+
+#LIGHTNING
+dtl[grepl('Summary', EVTYPE) & grepl('[Ll]ightning', REMARKS), EVTYPE:='LIGHTNING']
+
+#blizzards
+dtl[grepl('Summary', EVTYPE) & grepl('[Bb]lizzard', REMARKS), EVTYPE:='BLIZZARD']
+
+dtl[grepl('Summary', EVTYPE), REMARKS]
+
+
+
+
+#dtl[, MAG:=as.integer(MAG)]
