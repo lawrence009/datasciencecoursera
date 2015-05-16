8682480130e25ac5ca00045c7487304931791e96
Lawrence Chen <lawrence009@outlook.com>
2015-05-16 12:27:23 -0700

    storm data column classes

--- repdata/peer2/readData.R
+++ repdata/peer2/readData.R
@@ -1,49 +1,88 @@
 library(data.table)
 
+url <- 'https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2'
 filename <- 'repdata-data-StormData.csv.bz2'
 
-sample <- read.csv(filename, nrows = 100)
+# check if the data file is available
+# if (!file.exists(filename)) {
+#
+#     download.file(url = url, destfile = filename)
+#
+#     if (!require(R.utils)) {
+#         install.packages('R.utils')
+#         library(R.utils)  # a library for unzipping bz2 files
+#     }
+#     bunzip2(filename = filename,
+#             destname = 'repdata-data-StormData.csv')  # get the csv file for the task
+# }
 
-colClasses <- vapply(sample, class, 'character')
+
+
+sample1 <- read.csv(filename, na.strings = '', colClasses = 'character', nrows = 100)
+sample2 <- read.csv(filename, na.strings = '', nrows = 100)
+sample3 <- read.csv(filename, na.strings = '', nrows = 100, skip = 100000, header = F)
+colnames(sample3) <- colnames(sample1)
+
+colClasses2 <- vapply(sample2, class, 'character')
+colClasses3 <- vapply(sample3, class, 'character')
+
+ch2 <- grep('(logical|integer|factor)', colClasses2)
+colClasses2[ch2] <- 'character'
+
+as.data.frame(cbind(colClasses2, colClasses3))
+
+head(read.csv(filename, na.strings = '', colClasses = colClasses2, nrows = 100))
+
+
+head(sample1)
 
 #large data set; skip read if already exists in the environment
 if (!exists('dtl')) {
-    dtl <- as.data.table(read.csv(filename, na.strings = '', colClasses = 'character'))
+    dtl <- as.data.table(read.csv(filename, na.strings = '', colClasses = colClasses2))
 }
 
 
-dtl[1:10, as.Date(BGN_DATE, '%m/%d/%Y')]
+#dtl[1:10, as.Date(BGN_DATE, '%m/%d/%Y')]
 
-dtl[1:10, as.IDate(BGN_DATE, '%m/%d/%Y')]
+#dtl[1:10, as.IDate(BGN_DATE, '%m/%d/%Y')]
 
+#dtl[, c('FATALITIES','INJURIES','PROPDMG'):=list(as.numeric(FATALITIES), as.numeric(INJURIES), as.numeric(PROPDMG))]
 
-sort(dtl[, unique(EVTYPE)])
+dtl[, ':='(STATE__    =as.integer(STATE__),
+           BGN_DATE   =as.IDate(BGN_DATE, '%m/%d/%Y'),
+           REFNUM     =as.integer(REFNUM))]
 
-dtl[, EVTYPE:=toupper(EVTYPE)]
+#dtl[FATALITIES & 0 & INJURIES == 0 & PROPDMG == 0]
 
-#VOLCANIC ASH
-dtl[grepl('VO', EVTYPE), EVTYPE:='VOLCANIC ASH']
+#dtl[, EVTYPE:=toupper(EVTYPE)]
 
-#WATERSPOUT
+events <- dtl[, c('REFNUM', 'EVTYPE', 'REMARKS'), with=F]
 
+#F : F-Scale ; Fujita Scale
+
+# #VOLCANIC ASH
+# dtl[grepl('VO', EVTYPE), EVTYPE:='VOLCANIC ASH']
+#
+# #WATERSPOUT
+#
+
+#Summary
 #HUNDERSTORM WIND
-dtl[grepl('Summary', EVTYPE) & grepl('[Tt]hunderstorm.*[Ww]ind', REMARKS), EVTYPE:='THUNDERSTORM WIND']
+events[grepl('Summary', EVTYPE) & grepl('[Tt]hunderstorm.*[Ww]ind', REMARKS), EVTYPE:='THUNDERSTORM WIND']
 
 #HAIL
-dtl[grepl('Summary', EVTYPE) & grepl('[Hh]ail', REMARKS), EVTYPE:='HAIL']
+events[grepl('Summary', EVTYPE) & grepl('[Hh]ail', REMARKS), EVTYPE:='HAIL']
 
 #FLASH FLOOD
-dtl[grepl('Summary', EVTYPE) & grepl('[Ff]lash flood', REMARKS), EVTYPE:='FLASH FLOOD']
+events[grepl('Summary', EVTYPE) & grepl('[Ff]lash flood', REMARKS), EVTYPE:='FLASH FLOOD']
 
 #LIGHTNING
-dtl[grepl('Summary', EVTYPE) & grepl('[Ll]ightning', REMARKS), EVTYPE:='LIGHTNING']
+events[grepl('Summary', EVTYPE) & grepl('[Ll]ightning', REMARKS), EVTYPE:='LIGHTNING']
 
 #blizzards
-dtl[grepl('Summary', EVTYPE) & grepl('[Bb]lizzard', REMARKS), EVTYPE:='BLIZZARD']
+events[grepl('Summary', EVTYPE) & grepl('[Bb]lizzard', REMARKS), EVTYPE:='BLIZZARD']
 
-dtl[grepl('Summary', EVTYPE), REMARKS]
-
-
+events[grepl('Summary', EVTYPE), REMARKS]
 
 
-#dtl[, MAG:=as.integer(MAG)]
+sort(events[, unique(EVTYPE)])
